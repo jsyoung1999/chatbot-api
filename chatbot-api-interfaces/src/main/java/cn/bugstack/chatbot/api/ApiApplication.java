4680df3e2e4362f694e9c40809a8a6e813f08c7a
jsyoung1999 <2285510008@qq.com>
2023-04-03 14:08:30 +0800

    inti ddd project

--- chatbot-api-interfaces/src/main/java/cn/bugstack/chatbot/api/ApiApplication.java
+++ chatbot-api-interfaces/src/main/java/cn/bugstack/chatbot/api/ApiApplication.java
@@ -4,16 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @author 小傅哥，微信：fustack
+ * ApiApplication
+ *
+ * @author 22855
  * @description 启动入口
- * @github https://github.com/fuzhengwei
- * @Copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
+ * @date 2023/4/3 13:55
  */
 @SpringBootApplication
 public class ApiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ApiApplication.class, args);
-    }
-
+        public static void main(String[] args) {
+            SpringApplication.run(ApiApplication.class, args);
+        }
 }
