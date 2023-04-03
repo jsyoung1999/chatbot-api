40bec1c5b9a301aaacbbc718235a0ffd6e5e7d4d
jsyoung1999 <2285510008@qq.com>
2023-04-03 17:01:57 +0800

    知识星球接口领域服务开发

--- chatbot-api-interfaces/src/main/java/cn/bugstack/chatbot/api/ApiApplication.java
+++ chatbot-api-interfaces/src/main/java/cn/bugstack/chatbot/api/ApiApplication.java
@@ -4,16 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * ApiApplication
- *
- * @author 22855
+ * @author 小傅哥，微信：fustack
  * @description 启动入口
- * @date 2023/4/3 13:55
+ * @github https://github.com/fuzhengwei
+ * @Copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
  */
 @SpringBootApplication
 public class ApiApplication {
 
-        public static void main(String[] args) {
-            SpringApplication.run(ApiApplication.class, args);
-        }
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
 }
